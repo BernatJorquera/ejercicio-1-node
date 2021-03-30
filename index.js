f1e36618383cc608219093f8d1df710773cfc19c
Bernat Jorquera <bernatjv19@gmail.com>
2021-03-30 19:56:09 +0200

    Ruta realtiva des de donde se llama

--- index.js
+++ index.js
@@ -6,14 +6,13 @@ const args = process.argv;
 let nombre;
 for (const arg of args) {
   const parArgs = arg.split("=");
-  console.log(parArgs);
   if (parArgs[0] === "--nombre") {
     nombre = parArgs[1];
   }
 }
 
 if (nombre !== undefined && nombre.length) {
-  guardarSaludo(nombre);
+  guardarSaludo("./textos/saludo.txt", nombre);
 } else {
   console.log(chalk.yellow("No se ha introducido ningún nombre o se ha introducido un nombre en blanco. Abortando ejecución."));
   process.exit(1);
--- utilidades/archivos.js
+++ utilidades/archivos.js
@@ -1,8 +1,8 @@
 const fs = require("fs");
 const chalk = require("chalk");
 
-const guardarSaludo = (nombre) => {
-  fs.writeFile("C:/Users/Matí/Desktop/FE_Curs/C3/ejercicio-1-node/textos/saludo.txt", `Hola, ${nombre}. Encantado de saludarte.`, (err) => {
+const guardarSaludo = (ruta, nombre) => {
+  fs.writeFile(ruta, `Hola, ${nombre}. Encantado de saludarte.`, (err) => {
     if (err) {
       console.log(chalk.red.bold("¡¡¡HA HABIDO UN ERROR!!!\n") + err + chalk.yellow("\nAbortando ejecución."));
       process.exit(1);
